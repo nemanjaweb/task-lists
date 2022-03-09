91c0117b1a3bad23998a92f536b63b5e660d2558
abc <abc@example.com>
2022-03-09 11:38:16 +0100

    clear tasks function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Define UI Variables
+// Define UI Vars
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
@@ -8,35 +8,79 @@ const taskInput = document.querySelector("#task");
 // Load all event listeners
 loadEventListeners();
 
+// Load all event listeners
 function loadEventListeners() {
-  form.addEventListener("submit", addTask); //Add task event
+  // Add task event
+  form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+  // Clear task event
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
 }
 
-//Add Task
+// Add Task
 function addTask(e) {
   if (taskInput.value === "") {
-    alert("Add a task!");
+    alert("Add a task");
   }
 
-  //Create li element
+  // Create li element
   const li = document.createElement("li");
-  // Add class in materialize u have to add li class name same as ul class
+  // Add class
   li.className = "collection-item";
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement("a");
+  // Add class
   link.className = "delete-item secondary-content";
-  // Add Icon html
-  link.innerHTML = '<i class="fa fa-remove"></li>';
-  //Append the link to li
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
   li.appendChild(link);
 
-  //Append li to ul
+  // Append li to ul
   taskList.appendChild(li);
 
-  //Clear input
+  // Clear input
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
