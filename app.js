b278d3195c879ee91b2ed7cfc733831c7e10acc6
abc <abc@example.com>
2022-03-09 11:07:15 +0100

    html js

---
+++ app.js
@@ -0,0 +1,33 @@
+// Define UI Variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener("submit", addTask); //Add task event
+}
+
+//Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task!");
+  }
+
+  //Create li element
+  const li = document.createElement("li");
+  // Add class in materialize u have to add li class name same as ul class
+  li.className = "collection-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode("taskInput.value"));
+  // Create new link element
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  // Add Icon html
+  link.innerHTML = '<i class="fa fa-remove></li>';
+  e.preventDefault();
+}
